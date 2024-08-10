c47c816da21b292ed938f12a3bd68fa30526baff
bernheder <bernheder01@gmail.com>
2024-08-10 12:08:58 +0200

    catch errors in gridsearch and allow smaller condition matrices

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -26,7 +26,7 @@ def test_rectangle():
 
 def test_rectangle_consensus():
     sc_data, annotations, bulks = rectanglepy.load_tutorial_data()
-    sc_data = sc_data.iloc[:, :500]
+    sc_data = sc_data.iloc[:, :800]
     sc_data_adata = AnnData(sc_data, obs=annotations.to_frame(name="cell_type"))
 
     result = rectanglepy.rectangle_consens(
